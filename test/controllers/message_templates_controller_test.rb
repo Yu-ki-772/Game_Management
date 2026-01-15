db841c88326625412e0a051f68a2c4799c3adb8e
Yu-ki-772 <221048202+Yu-ki-772@users.noreply.github.com>
2026-01-15 14:52:26 +0900

    remove: 不要なテストファイルを削除

--- test/controllers/message_templates_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class MessageTemplatesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get message_templates_index_url
-    assert_response :success
-  end
-end
