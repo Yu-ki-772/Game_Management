c0e9ab45ba5cbee484c7e6d9d0b4b6518f915caf
Yu-ki-772 <221048202+Yu-ki-772@users.noreply.github.com>
2026-02-14 08:24:02 +0900

    feat: メール送信によるパスワードリセットの実装

--- config/routes.rb
+++ config/routes.rb
@@ -28,7 +28,8 @@ Rails.application.routes.draw do
   # ログイン認証
   #==============================================================
   devise_for :users, controllers: { registrations: "users/registrations",
-                                    omniauth_callbacks: "users/omniauth_callbacks" }
+                                    omniauth_callbacks: "users/omniauth_callbacks",
+                                    passwords: 'users/passwords' }
 
   #==============================================================
   # その他
