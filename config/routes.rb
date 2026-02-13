60d0b2371299fb978caf497cc558c5af7d52a595
Yu-ki-772 <221048202+Yu-ki-772@users.noreply.github.com>
2026-02-14 08:29:33 +0900

    style: Rubocopの指摘事項の修正

--- config/routes.rb
+++ config/routes.rb
@@ -29,7 +29,7 @@ Rails.application.routes.draw do
   #==============================================================
   devise_for :users, controllers: { registrations: "users/registrations",
                                     omniauth_callbacks: "users/omniauth_callbacks",
-                                    passwords: 'users/passwords' }
+                                    passwords: "users/passwords" }
 
   #==============================================================
   # その他
