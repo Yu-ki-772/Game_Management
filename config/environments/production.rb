aaf3f3acaad179c3c034f28730d485de1407a2f1
Yu-ki-772 <221048202+Yu-ki-772@users.noreply.github.com>
2026-02-25 09:14:38 +0900

    fix: キャッシュに関する問題

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,6 +87,8 @@ Rails.application.configure do
   # Only use :id for inspections in production.
   config.active_record.attributes_for_inspect = [ :id ]
 
+  config.active_record.use_schema_cache_dump = false
+
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
