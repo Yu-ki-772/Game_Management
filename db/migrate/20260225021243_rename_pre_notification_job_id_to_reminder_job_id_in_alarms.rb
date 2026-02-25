95859734495da7e6b1da5ce19f8c14b7be6434e2
Yu-ki-772 <221048202+Yu-ki-772@users.noreply.github.com>
2026-02-25 11:30:07 +0900

    fix: neon用のマイグレーションの改善・実行

--- db/migrate/20260225021243_rename_pre_notification_job_id_to_reminder_job_id_in_alarms.rb
+++ db/migrate/20260225021243_rename_pre_notification_job_id_to_reminder_job_id_in_alarms.rb
@@ -1,5 +1,7 @@
 class RenamePreNotificationJobIdToReminderJobIdInAlarms < ActiveRecord::Migration[8.1]
   def change
-    rename_column :alarms, :pre_notification_job_id, :reminder_job_id
+    if column_exists?(:alarms, :pre_notification_job_id)
+      rename_column :alarms, :pre_notification_job_id, :reminder_job_id
+    end
   end
 end
