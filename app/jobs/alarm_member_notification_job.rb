ec70fdcdd14b069983290ef564375f3044dc31e0
Yu-ki-772 <221048202+Yu-ki-772@users.noreply.github.com>
2026-03-01 15:34:39 +0900

    feature: アラーム通知をプッシュ通知に１本化

--- app/jobs/alarm_member_notification_job.rb
+++ app/jobs/alarm_member_notification_job.rb
@@ -4,9 +4,8 @@ class AlarmMemberNotificationJob < ApplicationJob
   queue_as :default
 
   retry_on Net::OpenTimeout, Net::ReadTimeout, wait: 5.seconds, attempts: 5
-  retry_on Resend::Error::RateLimitExceededError, wait: 5.seconds, attempts: 3
 
-  def perform(alarm_uuid, user_uuid, is_creator)
+  def perform(alarm_uuid, user_uuid)
     alarm = Alarm.find_by(uuid: alarm_uuid)
     return unless alarm
 
@@ -20,12 +19,6 @@ class AlarmMemberNotificationJob < ApplicationJob
 
     # メール・プッシュ通知より先に送信済みとして記録（エラー時のリトライによる二重送信対策）
     membership.update!(notified: true)
-
-    if is_creator
-      AlarmMailer.alarm_notification(alarm_uuid).deliver_now
-    else
-      AlarmMailer.member_alarm_notification(alarm_uuid, user_uuid).deliver_now
-    end
 
     user = User.find_by(uuid: user_uuid)
     return unless user
