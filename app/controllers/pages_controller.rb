b6aff71238979489922560dda2df302cbff2c848
Yu-ki-772 <221048202+Yu-ki-772@users.noreply.github.com>
2026-03-12 16:15:20 +0900

    style: rubocopの指摘事項の修正

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 # app/controllers/pages_controller.rb
 
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:privacy_policy, :terms]
+  skip_before_action :authenticate_user!, only: [ :privacy_policy, :terms ]
 
   def privacy_policy
   end
