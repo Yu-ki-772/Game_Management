b0e2d30f864c15970897a9c9970e882a35c731ec
Yu-ki-772 <221048202+Yu-ki-772@users.noreply.github.com>
2026-03-13 18:44:07 +0900

    web_push_subscription

---
+++ spec/models/web_push_subscription_spec.rb
@@ -0,0 +1,32 @@
+# spec/models/web_push_subscription_spec.rb
+require "rails_helper"
+
+RSpec.describe WebPushSubscription, type: :model do
+  subject { build(:web_push_subscription) }
+
+  # ============================================================
+  # アソシエーション
+  # ============================================================
+  describe "associations" do
+    it do
+      is_expected.to belong_to(:user)
+        .with_foreign_key(:user_uuid)
+        .with_primary_key(:uuid)
+    end
+  end
+
+  # ============================================================
+  # バリデーション
+  # ============================================================
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:endpoint) }
+    it { is_expected.to validate_presence_of(:p256dh) }
+    it { is_expected.to validate_presence_of(:auth) }
+
+    it do
+      is_expected.to validate_uniqueness_of(:endpoint)
+        .ignoring_case_sensitivity # endpoint はDBに保存時に小文字へ正規化される可能性があるため、大文字での一意性検証を無効化
+    end
+
+  end
+end
