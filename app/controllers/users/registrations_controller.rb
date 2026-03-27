3be9caa70f48f9e0e6b1724fb25183677874e7f8
Yu-ki-772 <221048202+Yu-ki-772@users.noreply.github.com>
2026-03-27 11:58:06 +0900

    fix: ストロングパラメータの設定を見直し

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -50,11 +50,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # protectedメソッド
   #=======================================================
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [ :attribute ])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :name, :uid, :provider ])
   end
 
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [ :attribute ])
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :name, :description, :avatar ])
   end
 
   def after_sign_up_path_for(resource)
