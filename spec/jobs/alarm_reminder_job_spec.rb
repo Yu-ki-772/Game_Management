4fe7e89c24f1b4a5b4ef2422a37dd8cfb970daca
Yu-ki-772 <221048202+Yu-ki-772@users.noreply.github.com>
2026-04-06 18:21:08 +0900

    アラームのジョブのspec

---
+++ spec/jobs/alarm_reminder_job_spec.rb
@@ -0,0 +1,31 @@
+# spec/jobs/alarm_reminder_job_spec.rb
+require "rails_helper"
+
+RSpec.describe AlarmReminderJob, type: :job do
+  let(:user)  { create(:user) }
+  let(:alarm) { create(:alarm, creator: user, user_uuid: user.uuid, scheduled_at: 30.minutes.from_now) }
+
+  # =========================================================
+  # ガード節: アラームが存在しないとき
+  # =========================================================
+  describe "アラームが存在しないとき" do
+    it "AlarmMemberReminderJobをエンキューしない" do
+      expect {
+        described_class.perform_now("存在しないuuid")
+      }.not_to have_enqueued_job(AlarmMemberReminderJob)
+    end
+  end
+
+
+
+  # =========================================================
+  # 正常系: 未通知のメンバーが存在するとき
+  # =========================================================
+  describe "未通知のメンバーが存在するとき" do
+    it "未通知のメンバー分だけAlarmMemberReminderJobをエンキューする" do
+      expect {
+        described_class.perform_now(alarm.uuid)
+      }.to have_enqueued_job(AlarmMemberReminderJob).with(alarm.uuid, user.uuid, 30)
+    end
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -82,4 +82,5 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
   config.include Devise::Test::IntegrationHelpers, type: :request
+  config.include ActiveJob::TestHelper, type: :job
 end
