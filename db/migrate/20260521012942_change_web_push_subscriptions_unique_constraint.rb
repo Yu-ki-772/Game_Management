0e1d8138399e76bd094573ec6fa77d9a6e004c55
Yu-ki-772 <221048202+Yu-ki-772@users.noreply.github.com>
2026-05-21 12:40:58 +0900

    chore: rubocopの指摘事項の修正

--- db/migrate/20260521012942_change_web_push_subscriptions_unique_constraint.rb
+++ db/migrate/20260521012942_change_web_push_subscriptions_unique_constraint.rb
@@ -1,6 +1,6 @@
 class ChangeWebPushSubscriptionsUniqueConstraint < ActiveRecord::Migration[8.1]
   def change
     remove_index :web_push_subscriptions, :endpoint
-    add_index :web_push_subscriptions, [:endpoint, :user_uuid], unique: true
+    add_index :web_push_subscriptions, [ :endpoint, :user_uuid ], unique: true
   end
 end
