c45113dcf3cd60d3415ac4ec725b017e5c6ad9a3
Yu-ki-772 <221048202+Yu-ki-772@users.noreply.github.com>
2026-05-23 18:31:39 +0900

    spec: リクエストスペック（web_push_subscriptions_spec）のhttpステータスコードの部分を修正

--- spec/requests/web_push_subscriptions_spec.rb
+++ spec/requests/web_push_subscriptions_spec.rb
@@ -66,16 +66,16 @@ RSpec.describe "WebPushSubscriptions" do
             params: { endpoint: subscription.endpoint },
             headers: { "Accept" => "text/vnd.turbo-stream.html" }
         }.to change(WebPushSubscription, :count).by(-1)
-        expect(response).to have_http_status(:no_content)
+        expect(response).to have_http_status(:ok)
       end
     end
 
     context "購読が存在しないとき" do
-      it "204を返す" do
+      it "200を返す" do
         delete web_push_subscriptions_path,
           params: { endpoint: "存在しないendpoint" },
           headers: { "Accept" => "text/vnd.turbo-stream.html" }
-        expect(response).to have_http_status(:no_content)
+        expect(response).to have_http_status(:ok)
       end
     end
 
